f61d37085a92c22d9d290f49abf41442a20665c8
edmgb <evangelos.dinopoulos@mgb.ch>
2022-08-23 14:31:45 +0200

    test fail

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,7 +5,7 @@ test('homepage has Playwright in title and get started link linking to the intro
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/failed/);
 
   // create a locator
   const getStarted = page.locator('text=Get Started');
